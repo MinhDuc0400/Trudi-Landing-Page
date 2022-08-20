a0b4d090ba22ca615aaad5f65fcc6464e40d6358
ducnm <minhduc0400@gmail.com>
2022-08-20 14:58:51 +0700

    add scroll and bubble

--- trudi landing/index.js
+++ trudi landing/index.js
@@ -1,17 +1,40 @@
+let activeId;
+
 function clickButton(id) {
+    activeId = id;
     const listButton = document.querySelectorAll(".round-end-button-shape");
+    const elementShowing = document.getElementById(`section${id}`);
+    changeSrc(id)
     listButton.forEach((button, idx) => {
         if (idx === id) {
             if (button.classList.contains('long-button-inactive')) {
                 button.classList.remove('long-button-inactive')
             }
             button.classList.add('long-button-active')
+            changeSrc(idx, true)
         } else {
             if (button.classList.contains('long-button-active')) {
                 button.classList.remove('long-button-active')
             }
             button.classList.add('long-button-inactive')
+            changeSrc(idx, false)
         }
         }
-    )
+    );
+    elementShowing.scrollIntoView({behavior: "smooth", block: "center"})
 }
+
+function changeSrc(id, isActive) {
+    const img = document.getElementById(`img${id}`)
+    let pathImgArray = img.src.split('/');
+    const fileNameArray = pathImgArray[pathImgArray.length - 1].split('.')
+    if (isActive) {
+        fileNameArray[0] = fileNameArray[0].includes('_active') ? fileNameArray[0] : fileNameArray[0] + '_active';
+    } else {
+        fileNameArray[0] = !fileNameArray[0].includes('_active') ? fileNameArray[0] : fileNameArray[0].replace('_active', '')
+    }
+    pathImgArray[pathImgArray.length - 1] = fileNameArray.join('.');
+    img.src = pathImgArray.join('/')
+}
+
+clickButton(0);
